f9afdfb3bd474ab92dd1866ff370f9c703745b03
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-04-18 09:16:23 +0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 
+import "./app.css";
+
 const App = () => {
   const api_call = "7973f7c02b999b1d3504034b5a019ef1";
   const api_url = "https://api.openweathermap.org/data/2.5/weather?"
 
-  const [data, setdata] = useState({main : {temp : Number , humidity : Number , feels_like : Number , pressure : Number} , wind : {speed : Number}});
+  const [data, setdata] = useState({ main: { temp: Number, humidity: Number, feels_like: Number, pressure: Number }, wind: { speed: Number } });
 
 
   useEffect(() => {
@@ -29,9 +31,29 @@ const App = () => {
 
 
       {data !== {} ?
-        <p>
-          Temp : {data.main.temp} Humidity : {data.main.humidity} Pressure : {data.main.pressure} Feels Like : {data.main.feels_like} Wind Speed : {data.wind.speed}
-        </p>
+
+
+
+
+        <div className="parent" >
+
+
+          <div className="temperature" > <h1>{data.main.temp} </h1> </div>
+
+          <div className="city" > <h6>{data.name} </h6> </div>
+          <div className="extra">
+
+            <span>  <p> Feels Like : {data.main.feels_like} </p> </span>
+            <span>  <p> Presure : {data.main.pressure} </p></span>
+            <span>  <p> Humidity : {data.main.humidity}  </p></span>
+            <span>  <p> Wind : {data.wind.speed} </p></span>
+
+          </div>
+
+
+
+        </div>
+
         :
         null
       }
